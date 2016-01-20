1b35d3f8e33a7a0c6bca9e77e52a4d5132df50c3
Yauheni Parkhomenko <egenpark@gmail.com>
2016-01-20 22:08:16 +0300

    test commit

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -14,6 +14,7 @@ public class IntegrationTest {
     /**
      * add your integration test here
      * in this example we just check if the welcome page is being shown
+     test lol
      */
     @Test
     public void test() {
